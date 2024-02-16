9f92349e51875f3c85f292944f9d42fd1a147854
Vladimir Sorokin <sorokin.dev@mail.ru>
2024-02-16 12:20:51 +0300

    Implement basic functionality to create habits and select dates

---
+++ android/app/src/main/kotlin/ru/yandex_edu/habits_arch_demo/MainActivity.kt
@@ -0,0 +1,6 @@
+package ru.yandex_edu.habits_arch_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/habit_tile.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+import 'habit.dart';
+import 'weekly_calendar.dart';
+
+class HabitTile extends StatelessWidget {
+  final Habit habit;
+  final Function(DateTime) onTapDate;
+  final VoidCallback onTapHabit;
+
+  const HabitTile({
+    required this.habit,
+    required this.onTapDate,
+    required this.onTapHabit,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        InkWell(
+          onTap: onTapHabit,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 16,
+            ),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Text(
+                    habit.title,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+                const Icon(Icons.chevron_right),
+              ],
+            ),
+          ),
+        ),
+        const SizedBox(height: 4),
+        WeeklyCalendar(
+          selectedDates: habit.completedDates,
+          onTapDate: onTapDate,
+        ),
+      ],
+    );
+  }
+}
