f84f5561f1aa4199bf863106cf8b3a328ebb51a5
Vladimir Sorokin <sorokin.dev@mail.ru>
2024-02-19 10:12:02 +0300

    Use state holder in habit show

--- lib/ui/screens/habit_show/habit_show_screen.dart
+++ lib/ui/screens/habit_show/habit_show_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:habits_arch_demo/domain/habit_state_holder.dart';
 
 import '../../widgets/weekly_calendar.dart';
 
@@ -17,38 +18,45 @@ class HabitShowScreen extends StatefulWidget {
 class _HabitShowScreenState extends State<HabitShowScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(_habit.title),
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(16),
-            child: Text(
-              'Habit progress: ${_habit.completedDates.length}',
-              style: const TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.w500,
-              ),
+    return ListenableBuilder(
+        listenable: HabitsStateHolder.instance,
+        builder: (context, _) {
+          final habit = HabitsStateHolder.instance.getHabitById(widget.habitId);
+          if (habit == null) {
+            return Center(
+              child: Text('Error, no such habit'),
+            );
+          }
+          return Scaffold(
+            appBar: AppBar(
+              backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+              title: Text(habit.title),
             ),
-          ),
-          WeeklyCalendar(
-            selectedDates: _habit.completedDates,
-            onTapDate: (date) {
-              setState(() {
-                if (_habit.completedDates.contains(date)) {
-                  _habit.completedDates.remove(date);
-                } else {
-                  _habit.completedDates.add(date);
-                }
-              });
-            },
-          ),
-        ],
-      ),
-    );
+            body: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Text(
+                    'Habit progress: ${habit.completedDates.length}',
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+                WeeklyCalendar(
+                  selectedDates: habit.completedDates,
+                  onTapDate: (date) {
+                    HabitsStateHolder.instance.toggleDateForHabit(
+                      habitId: habit.id,
+                      date: date,
+                    );
+                  },
+                ),
+              ],
+            ),
+          );
+        });
   }
 }
